3f2c3931f56d51660badc3f18012212473aca016
Hawyar <hawyarxoshnaw@gmail.com>
2020-02-28 10:13:25 -0500

    Initial commit

---
+++ Chapter 3/3.1/checkpoint_4/Main.java
@@ -0,0 +1,17 @@
+package chapter3;
+
+/*
+ * Write a Java Program that prints your first
+ * name on the first line and your last name
+ * on the second line
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hawyar");
+        System.out.println("Farooq");
+
+    }
+}
